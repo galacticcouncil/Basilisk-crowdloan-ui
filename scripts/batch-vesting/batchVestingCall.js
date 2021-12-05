4ea9844eed31750da5d00b3ddbf3946693c30d8d
Lumir Mrkva <mrq@mrq.cz>
2021-12-05 16:19:22 +0100

    setting correct weight limit

--- scripts/batch-vesting/batchVestingCall.js
+++ scripts/batch-vesting/batchVestingCall.js
@@ -2,7 +2,6 @@ require("dotenv").config()
 const BN = require("bn.js")
 const {ApiPromise, WsProvider, Keyring} = require("@polkadot/api")
 const {encodeAddress, cryptoWaitReady} = require("@polkadot/util-crypto")
-const types = require("../types.js")
 const {stringToU8a} = require("@polkadot/util")
 
 const vestings = require("./data/vestings.json")
@@ -11,25 +10,29 @@ const assert = require("assert");
 const ACCOUNT_SECRET = process.env.ACCOUNT_SECRET || "//Alice"
 const RPC = process.env.RPC_SERVER || "ws://127.0.0.1:9988"
 
-const amountToBN = (amount) => new BN(hdx).mul(new BN(10).pow(new BN(12)))
 const bsxAddress = (pubKey) => encodeAddress(pubKey, 10041) // https://wiki.polkadot.network/docs/build-ss58-registry
 const chunkify = (a, size) => Array(Math.ceil(a.length / size))
   .fill()
   .map((_, i) => a.slice(i * size, i * size + size));
+const sendAndWaitFinalization = ({from, tx, printEvents = []}) => new Promise(resolve =>
+  tx.signAndSend(from, ({status, events = []}) => {
+    if (status.isInBlock) {
+      console.log('included in', status.asInBlock.toHex());
+      events.filter(({event: {section}}) => printEvents.includes(section))
+        .forEach(({ event: { data, method, section } }) =>
+          console.log(`${section}.${method}`, data.toHuman()));
+    }
+    if (status.isFinalized) {
+      console.log('finalized', status.asFinalized.toHex());
+      resolve();
+    }
+  }));
 
 async function main() {
   await cryptoWaitReady()
   const provider = new WsProvider(RPC)
   const keyring = new Keyring({type: "sr25519"})
-  const api = await ApiPromise.create({
-    provider: provider,
-    types: types,
-    typesAlias: {
-      tokens: {
-        AccountData: "OrmlAccountData",
-      },
-    },
-  })
+  const api = await ApiPromise.create({provider});
 
   const [chain, nodeVersion] = await Promise.all([
     api.rpc.system.chain(),
@@ -39,7 +42,6 @@ async function main() {
 
   const from = keyring.addFromUri(ACCOUNT_SECRET)
   console.log("sudo account:", bsxAddress(from.addressRaw))
-
 
   const treasuryPubKey = stringToU8a("modlpy/trsry".padEnd(32, "\0"))
   const TREASURY = bsxAddress(treasuryPubKey)
@@ -54,12 +56,13 @@ async function main() {
   const batch = api.tx.utility.batch(vestingSchedules)
   const sudo = api.tx.sudo.sudoAs(TREASURY, batch)
 
-  const {maxBlock} = api.consts.system.blockWeights;
+  let {maxExtrinsic: weightLimit} = api.consts.system.blockWeights.perClass.normal;
   const {weight} = await sudo.paymentInfo(from);
   console.log('weight of the whole batch', weight.toHuman());
-  console.log('max block weight ', maxBlock.toHuman());
+  console.log('weight limit', weightLimit.toHuman());
+  weightLimit = new BN(weightLimit.toString());
 
-  const blocks = weight.div(maxBlock).toNumber() + 1;
+  const blocks = weight.div(weightLimit).toNumber() + 1;
   console.log(`batch have to be split into ${blocks} blocks`);
 
   const vestingsPerBlock = Math.ceil(vestingSchedules.length / blocks);
@@ -69,33 +72,32 @@ async function main() {
   const weights = await Promise.all(
     chunks.map(async chunk => {
       const {weight} = await chunk.paymentInfo(from);
-      assert(weight.lt(maxBlock), `chunk overweight: ${weight}`);
+      assert(weight.lt(weightLimit), `chunk overweight: ${weight}`);
       return weight;
     })
   );
 
-  console.log(`chunk weight ${weights[0].toHuman()} < ${maxBlock.toHuman()}`);
+  console.log(`chunk weight ${weights[0].toHuman()}`);
 
   if (process.argv[2] === "test") {
     console.log('run "npm start" to send tx')
     process.exit()
   }
+  const startFrom = Number(process.argv[2]) || 0;
 
-  console.log("sending txs")
-  const sendAndWaitFinalization = tx => new Promise(resolve =>
-    tx.signAndSend(from, ({status}) => {
-      if (status.isInBlock) console.log('included in', status.asInBlock.toHex())
-      if (status.isFinalized) resolve()
-    }));
-
-  for (let i = 0; i < chunks.length; i++) {
+  console.log("sending txs");
+  for (let i = startFrom; i < chunks.length; i++) {
     console.log('batch', i);
-    await sendAndWaitFinalization(chunks[i]).catch(e => {
+    await sendAndWaitFinalization({
+      from,
+      tx: chunks[i],
+      printEvents: ["utility", "sudo"]
+    }).catch(e => {
       console.log(e);
       process.exit(1);
     });
-    console.log('done')
   }
+
   process.exit(0);
 }
 
