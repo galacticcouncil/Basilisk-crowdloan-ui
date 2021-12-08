72b82f0eb0d9ba1fdc5c07f670e33c1a1c26999b
timothydm42 <m.timothyd@gmail.com>
2021-12-08 09:19:41 -0700

    feedback fixes

--- scripts/airdropBsxToHdx/getAccounts.ts
+++ scripts/airdropBsxToHdx/getAccounts.ts
@@ -4,12 +4,21 @@
 // ✅ -> discard treasury and intergalactic acc
     // 0x6d6f646c70792f74727372790000000000000000000000000000000000000000 (HDX Treasury)
     // 0x0abad795adcb5dee45d29528005b1f78d55fc170844babde88df84016c6cd14d (Founders & Investors)
+// ✅ for an individual account sum free and reserved balance 💪
+// ✅ total all balances for use in calculating airdrop according to weight 
 
 // ✅ create an 'accounts' .json ?  probably.. like 'contribs' for documentation purposes :| :) 
+
 
 const { ApiPromise, WsProvider, Keyring } = require("@polkadot/api")
 const { deriveAddress, encodeAddress, cryptoWaitReady } = require("@polkadot/util-crypto")
 const { stringToU8a } = require("@polkadot/util")
+const BN = require("bignumber.js")
+
+interface AirdropBsxToHdxVestingData {
+    totalAllOgBalances: string
+    OgAccounts: OgAccount[]
+}
 
 interface OgAccount {
     address: string
@@ -39,6 +48,7 @@ async function main() {
         `
     )
     
+    let totalAllOgBalances = new BN('0')
     const blockHeight = '1999999'
     //   ^^^  block height at which to get orignial HDX accounts
     //   link or explanation for this blockheight ?
@@ -49,28 +59,40 @@ async function main() {
             .then( signedBlock => signedBlock.block.header.hash.toHex() )
             .then( blockHex => api.query.system.account.entriesAt(blockHex) )
             .then( accountsData => accountsData.reduce(( acctsWeWant, acct ) => {
-                const acctAddress = hdxAddress(acct[0].slice(-32))
+                const address = hdxAddress(acct[0].slice(-32))
+                // ^^^ * see explanation below
                 if ( 
-                    acctAddress === treasuryHdxAcct
-                    || acctAddress === foundersInvestorsHdxAcct
+                    address === treasuryHdxAcct
+                    || address === foundersInvestorsHdxAcct
                 ) {
-                    console.log('no airdrop for: ', acctAddress)
+                    console.log('no airdrop for: ', address)
                     return acctsWeWant // we return the accounts we want _without_ adding treasury or founders/investors
                 } else {
+                    const balanceTotal = new BN(acct[1].data.free.toString()) 
+                                                .plus( new BN(acct[1].data.reserved.toString()))
+                    
+                    totalAllOgBalances = totalAllOgBalances.plus( new BN( balanceTotal ) )
+                    
                     acctsWeWant.push({
-                        address: acctAddress,
-                        freeBalance: acct[1].data.free.toString(),
-                        // reservedBalance: acct[1].data.reserved.toString()
-                        // question:  reserved balance doesn't matter,  right ?
+                        address,
+                        balanceTotal,
+                            // ^^^ Note:  free balance includes frozen balance
+                            // https://wiki.polkadot.network/docs/learn-accounts#balance-types
                     })
                     return acctsWeWant
                 }
             }, [] ) )
 
+
+    const airdropBsxToHdxVestingData: AirdropBsxToHdxVestingData = {
+        totalAllOgBalances,
+        OgAccounts: ogHdxAccts
+    }  
+    
     const fs = require('fs');
     fs.writeFile(
         "./data/ogHdxHodlers.json", 
-        JSON.stringify(ogHdxAccts, null, 4), 
+        JSON.stringify(airdropBsxToHdxVestingData, null, 4), 
         { flag: 'wx' }, 
         function(err: any) {
             if (err) {
@@ -89,3 +111,21 @@ main().catch((e) => {
 
 function hdxAddress( pubKey ) { return encodeAddress(pubKey, 63) }
 
+/**
+ * I found that `account.slice(-32)` thing here: https://stackoverflow.com/a/66228892
+
+    And because that looks hacky ,  I spent some time trying to understand and use different utility methods,  but gave up after a while
+
+    as the code stands now,  if I don't do the `.slice(-32)`,  It throws:  
+
+    ```
+    Error: Expected a valid key to convert, with length 1, 2, 4, 8, 32, 33
+        at assert (/Users/timothymitchell/dev/gc/repos/Basilisk-crowdloan-ui/node_modules/@polkadot/util/assert.cjs:36:11)
+        at encodeAddress (/Users/timothymitchell/dev/gc/repos/Basilisk-crowdloan-ui/node_modules/@polkadot/util-crypto/address/encode.cjs:26:20)
+        at hdxAddress (/Users/timothymitchell/dev/gc/repos/Basilisk-crowdloan-ui/scripts/airdropBsxToHdx/getAccounts.ts:94:40)
+        at /Users/timothymitchell/dev/gc/repos/Basilisk-crowdloan-ui/scripts/airdropBsxToHdx/getAccounts.ts:54:37
+        at Array.reduce (<anonymous>)
+        at /Users/timothymitchell/dev/gc/repos/Basilisk-crowdloan-ui/scripts/airdropBsxToHdx/getAccounts.ts:52:49
+        at processTicksAndRejections (node:internal/process/task_queues:96:5)
+    ```
+ */
